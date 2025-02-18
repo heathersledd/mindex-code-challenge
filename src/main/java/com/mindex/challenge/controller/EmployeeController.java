82555df0a76cf3fe7a5886439e841eee968ff80a
mindex <dev@mindex.com>
2025-02-18 14:36:00 -0700

    Adds reporting structure endpoint and renames controller and service methods.

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,32 +13,44 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for managing employees.
+ * Provides the endpoints for creating, reading, and updating employee data.
+ */
 @RestController
 public class EmployeeController {
+
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping("/employee/{employeeId}")
+    public Employee getEmployee(@PathVariable String employeeId) {
+        LOG.debug("Received employee get request for id [{}]", employeeId);
+
+        return employeeService.getEmployee(employeeId);
+    }
+
+    @GetMapping("/reportingStructure/{employeeId}")
+    public ReportingStructure getReportingStructure(@PathVariable String employeeId) {
+        LOG.debug("Received reporting structure get request for employee with id [{}]", employeeId);
+
+        return employeeService.getReportingStructure(employeeId);
+    }
+
     @PostMapping("/employee")
-    public Employee create(@RequestBody Employee employee) {
+    public Employee createEmployee(@RequestBody Employee employee) {
         LOG.debug("Received employee create request for [{}]", employee);
 
-        return employeeService.create(employee);
+        return employeeService.createEmployee(employee);
     }
 
-    @GetMapping("/employee/{id}")
-    public Employee read(@PathVariable String id) {
-        LOG.debug("Received employee create request for id [{}]", id);
+    @PutMapping("/employee/{employeeId}")
+    public Employee updateEmployee(@PathVariable String employeeId, @RequestBody Employee employee) {
+        LOG.debug("Received employee update request for id [{}] and employee [{}]", employeeId, employee);
 
-        return employeeService.read(id);
-    }
-
-    @PutMapping("/employee/{id}")
-    public Employee update(@PathVariable String id, @RequestBody Employee employee) {
-        LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
-
-        employee.setEmployeeId(id);
-        return employeeService.update(employee);
+        employee.setEmployeeId(employeeId);
+        return employeeService.updateEmployee(employee);
     }
 }
